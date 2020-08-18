22c90c74486a6fb2e261459f30f256322a53a55c
João Rocha da Silva <joaorosilva@gmail.com>
2020-08-18 19:19:47 +0100

    minimum-window-substring.js in progress

--- src/minimum-window-substring/minimum-window-substring.js
+++ src/minimum-window-substring/minimum-window-substring.js
@@ -5,47 +5,65 @@
 var getCharsDict = function (string) {
     let charsDict = {};
     for (let i = 0; i < string.length; i++) {
-        charsDict[string[i]] = 1;
+        if (!charsDict[string[i]])
+            charsDict[string[i]] = 1;
+        else
+            charsDict[string[i]]++;
     }
     return charsDict;
 }
 
+var registerWindow = function (s, tailPos, headPos, smallestWindow) {
+    let aWindow = s.substring(tailPos, headPos + 1)
+
+    if ((smallestWindow === "" || aWindow.length < smallestWindow.length)) {
+        return aWindow;
+    } else
+        return smallestWindow;
+}
+
 var search = function (s, searchDict) {
     let smallestWindow = "";
-    let aWindow = "";
     let missingChars = JSON.parse(JSON.stringify(searchDict));
     let missingCharsCount = Object.keys(searchDict).length;
     let tailPos = 0;
     let headPos = 0;
 
-    while (headPos < s.length && tailPos < s.length) {
+    while (headPos < s.length || tailPos < s.length) {
         while (headPos < s.length) {
-            if (searchDict[s.charAt(headPos)])
-                missingChars[s.charAt(headPos)]--;
-            if (missingChars[s.charAt(headPos)] === 0)
-                missingCharsCount--;
-            if (missingCharsCount === 0)
-                break;
-            else
+            if (!searchDict[s.charAt(headPos)])
                 headPos++;
+            else {
+                missingChars[s.charAt(headPos)]--;
+                if (missingChars[s.charAt(headPos)] === 0)
+                    missingCharsCount--;
+                if (missingCharsCount === 0) {
+                    smallestWindow = registerWindow(s, tailPos, headPos, smallestWindow);
+                    headPos++;
+                    break;
+                } else
+                    headPos++;
+            }
         }
 
         while (tailPos < s.length) {
-            aWindow = s.substring(tailPos, headPos + 1)
-            if (missingCharsCount === 0 && (smallestWindow === "" || aWindow.length < smallestWindow.length))
-                smallestWindow = aWindow;
-
-            if (searchDict[s.charAt(tailPos)])
-                missingChars[s.charAt(tailPos)]++;
-            if (missingChars[s.charAt(tailPos)] === 1)
-                missingCharsCount++;
-
-            if (tailPos < headPos)
-                tailPos++;
-            else if (headPos === s.length - 1)
-                tailPos++;
-            else
+            if (tailPos === headPos) {
                 break;
+            } else if (searchDict[s.charAt(tailPos)]) {
+                if (missingCharsCount === 0) {
+                    smallestWindow = registerWindow(s, tailPos, headPos, smallestWindow);
+                    missingChars[s.charAt(tailPos)]++;
+                    if (missingChars[s.charAt(tailPos)] === 1) {
+                        missingCharsCount++;
+                    }
+                    tailPos++;
+                } else if (headPos === s.length)
+                    tailPos++;
+                else
+                    break;
+            } else {
+                tailPos++;
+            }
         }
     }
 
@@ -56,21 +74,13 @@ var minWindow = function (s, t) {
     const searchDict = getCharsDict(t);
     if (s === t) {
         return s;
-    }
-    return search(s, searchDict);
+    } else if (t.length > s.length)
+        return "";
+    else if (t.length === 1)
+        if (s.indexOf(t[0]) > -1)
+            return t;
+        else
+            return "";
+    else
+        return search(s, searchDict);
 };
-
-
-console.log(JSON.stringify(minWindow("ADOBECODEBANC", "ABC")));
-
-// console.log(JSON.stringify(minWindow("AKBCDBBA", "BA")));
-// console.log(JSON.stringify(minWindow("A", "A")));
-// console.log(JSON.stringify(minWindow("AB", "AB")));
-// console.log(JSON.stringify(minWindow("ABC", "AC")));
-// console.log(JSON.stringify(minWindow("ABBC", "ABC")));
-// console.log(JSON.stringify(minWindow("ABBCD", "AD")));
-// console.log(JSON.stringify(minWindow("ABBCD", "ABC")));
-// console.log(JSON.stringify(minWindow("ABACCA", "AC")));
-// console.log(JSON.stringify(minWindow("ABBCD", "ABCD")));
-
-
