36fbf4a2f40cbf414b9149daa581e55a43060e3b
João Rocha da Silva <joaorosilva@gmail.com>
2020-08-22 17:38:26 +0100

    added tests for longest-valid-parenthesis problem
    https://leetcode.com/problems/longest-valid-parentheses/

--- src/longest-valid-parentheses/longest-valid-parentheses.js
+++ src/longest-valid-parentheses/longest-valid-parentheses.js
@@ -2,8 +2,40 @@
  * @param {string} s
  * @return {number}
  */
-var longestValidParentheses = function(s) {
 
+class Teleport {
+    origin;
+    destination;
+    character;
+
+    constructor(origin, destination, character) {
+        this.origin = origin;
+        this.destination = destination;
+        this.character = character;
+    }
+}
+
+var longestValidParentheses = function(s) {
+    if (s.length === 0)
+        return 0;
+    else if (s.length === 1)
+        return 0;
+    else {
+        let h = 0;
+        let t = 0;
+        let bh = 0;
+        let bt = 0;
+        const teleports = {};
+        const stack = [];
+        return;
+        while (h < s.length && t < s.length) {
+            if (s.charAt(h) === "(") {
+
+            } else if (s.charAt(h) === ")") {
+
+            }
+        }
+    }
 };
 
 exports.longestValidParentheses = longestValidParentheses;
--- src/longest-valid-parentheses/longest-valid-parenthesis.test.js
+++ src/longest-valid-parentheses/longest-valid-parenthesis.test.js
@@ -10,12 +10,9 @@ describe('Longest Valid Parenthesis', function () {
             assert.equal(longestValidParentheses(""), 0);
         });
     });
-    describe('Valid Cases', function () {
-        it('Input: (), output = 2', function () {
-            assert.equal(longestValidParentheses("()"), 2);
-        });
-        it('Input: ()(), output = 4', function () {
-            assert.equal(longestValidParentheses("()()"), 4);
+    describe('Open open close close', function () {
+        it('Input: (()), output = 4', function () {
+            assert.equal(longestValidParentheses("(())"), 4);
         });
         it('Input: ((((())))), output = 10', function () {
             assert.equal(longestValidParentheses("((((()))))"), 10);
@@ -23,42 +20,60 @@ describe('Longest Valid Parenthesis', function () {
         it('Input: (((((()))))), output = 12', function () {
             assert.equal(longestValidParentheses("(((((())))))"), 12);
         });
-        it('Input: ()()()()()(), output = 12', function () {
-            assert.equal(longestValidParentheses("()()()()()()"), 12);
-        });
-        it('Input: ()[]{}, output = 6', function () {
-            assert.equal(longestValidParentheses("()[]{}"), 6);
-        });
-        it('Input: {[]}, output = 4', function () {
-            assert.equal(longestValidParentheses("{[]}"), 4);
-        });
-        it('Input: ()()()()()( , output = 10', function () {
-            assert.equal(longestValidParentheses("()()()()()("), 10);
-        });
-        it('Input: )()()()()( , output = 8', function () {
-            assert.equal(longestValidParentheses(")()()()()("), 8);
-        });
-        it('Input: )()[]{}()( , output = 8', function () {
-            assert.equal(longestValidParentheses(")()()()()("), 8);
-        });
-        it('Input: ()[]{}()( , output = 8', function () {
-            assert.equal(longestValidParentheses("()[]{}()("), 8);
-        });
-        it('Input: ()[]{}() , output = 8', function () {
-            assert.equal(longestValidParentheses("()[]{}()"), 8);
-        });
-        it('Input: [[[[[[[[]]]]]]]]]]]]]]]]]]] , output = false', function () {
-            assert.equal(longestValidParentheses("[[[[[[[[]]]]]]]]]]]]]]]]]]]"), 2);
-        });
-    });
-
-    describe('Invalid Cases', function () {
-        it('Input: { , output = 0', function () {
+        it('Input: ( , output = 0', function () {
             assert.equal(longestValidParentheses("{"), 0);
         });
         it('Input: } , output = 0', function () {
             assert.equal(longestValidParentheses("}"), 0);
         });
+    });
+
+    describe('Open and close, open and close', function () {
+        it('Input: (), output = 2', function () {
+            assert.equal(longestValidParentheses("()"), 2);
+        });
+        it('Input: ()(), output = 4', function () {
+            assert.equal(longestValidParentheses("()()"), 4);
+        });
+        it('Input: ()()(), output = 6', function () {
+            assert.equal(longestValidParentheses("()[]{}"), 6);
+        });
+        it('Input: ()()()() , output = 8', function () {
+            assert.equal(longestValidParentheses("()[]{}()"), 8);
+        });
+        it('Input: ()()()()()(), output = 12', function () {
+            assert.equal(longestValidParentheses("()()()()()()"), 12);
+        });
+    });
+
+    describe('Open and close, open and close and junk at start or end', function () {
+        it('Input: ()()()()()( , output = 10', function () {
+            assert.equal(longestValidParentheses("()()()()()("), 10);
+        });
+        it('Input: )()()()() , output = 8', function () {
+            assert.equal(longestValidParentheses("()()()()("), 8);
+        });
+        it('Input: )()()()()( , output = 8', function () {
+            assert.equal(longestValidParentheses(")()()()()("), 8);
+        });
+        it('Input: )()()()()(, output = 8', function () {
+            assert.equal(longestValidParentheses(")()()()()("), 8);
+        });
+        it('Input: (((((()))))))))))))))))) , output = false', function () {
+            assert.equal(longestValidParentheses("(((((())))))))))))))))))"), 12);
+        });
+    });
+
+    describe('Open and close, open and close and junk at middle', function () {
+        it('Input: ()())))))-->()()()<--( , output = 6', function () {
+            assert.equal(longestValidParentheses("()())))))()()()("), 6);
+        });
+        it('Input: ()()((((((-->()()()<--( , output = 6', function () {
+            assert.equal(longestValidParentheses("()()((((((()()()("), 6);
+        });
+    });
+
+    describe('Invalid Cases', function () {
         it('Input: (], output = 0', function () {
             assert.equal(longestValidParentheses("(]"), 0);
         });
