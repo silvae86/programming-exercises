61a26f7f12675ff46bcada4ff5e6ae46e99e06d6
João Rocha da Silva <joaorosilva@gmail.com>
2020-08-27 13:47:56 +0100

    Maximal rectangle implemented with DFS... Too slow!
    https://leetcode.com/problems/maximal-rectangle/

--- src/maximal-rectangle/maximal-rectangle.js
+++ src/maximal-rectangle/maximal-rectangle.js
@@ -1,9 +1,73 @@
+let convertPosition = function (r, c, direction) {
+    switch (direction) {
+        case("right"):
+            return {r: r, c: c + 1};
+        case("down"):
+            return {r: r + 1, c: c};
+    }
+};
+
+let nextValidPositions = function (matrix, r, c, directions) {
+    const validPositions = [];
+    for (let i = 0; i < Object.keys(directions).length; i++) {
+        let key = Object.keys(directions)[i];
+        if (directions[key]) {
+            const newValidPosition = convertPosition(r, c, key);
+            validPositions.push(newValidPosition);
+        }
+    }
+    return validPositions;
+};
+
+
+let getMaxRectangle = function (matrix, r, c, directions) {
+    console.log(`Checking (r: ${r}, c: ${c})`);
+    let currentRectArea = 1;
+
+    if (!directions) {
+        directions = {};
+        directions.right = (c < matrix[r].length - 1) && matrix[r][c + 1] === "1";
+        directions.down = (r < matrix.length - 1) && matrix[r + 1][c] === "1";
+    } else {
+        directions.right = directions.right && (c < matrix[r].length - 1) && matrix[r][c + 1] === "1";
+        directions.down = directions.down && (r < matrix.length - 1) && matrix[r + 1][c] === "1";
+    }
+
+    let nextViablePositions = nextValidPositions(matrix, r, c, directions);
+
+    if (nextViablePositions.length === 0) {
+        return 1;
+    } else {
+        for (let i = 0; i < nextViablePositions.length; i++) {
+            let nextPos = nextViablePositions[i];
+            currentRectArea = currentRectArea + getMaxRectangle(matrix, nextPos.r, nextPos.c, directions)
+        }
+    }
+
+    return currentRectArea;
+
+};
+
+
 /**
- * @param {character[][]} matrix
+ * @param {string[][]} matrix
  * @return {number}
  */
 var maximalRectangle = function (matrix) {
+    let maxArea = 0;
+    for (let r = 0; r < matrix.length; r++) {
+        let row = matrix[r];
+        for (let c = 0; c < row.length; c++) {
+            if (matrix[r][c] === "1") {
+                let areaOfNextRectangle = getMaxRectangle(matrix, r, c);
+                if (maxArea < areaOfNextRectangle) {
+                    maxArea = areaOfNextRectangle;
+                }
+            }
+        }
+    }
 
+    return maxArea;
 };
 
 exports.maximalRectangle = maximalRectangle;
--- src/maximal-rectangle/maximal-rectangle.test.js
+++ src/maximal-rectangle/maximal-rectangle.test.js
@@ -10,6 +10,29 @@ describe('Maximal Rectangle', function () {
             assert.equal(maximalRectangle([]), 0);
         });
     });
+
+    describe('Valid Cases', function () {
+        it('[Single island in middle] Input: [ ["0","0","0","0","0"], ["0","1","1","1","0"], ["0","1","1","1","0"], ["0","0","0","0","0"]", output = 6', function () {
+            assert.equal(maximalRectangle([["0", "0", "0", "0", "0"], ["0", "1", "1", "1", "0"], ["0", "1", "1", "1", "0"], ["0", "0", "0", "0", "0"]]), 6);
+        });
+        it('[Two islands in middle] Input: [ ["0","0","0","0","0"], ["0","1","0","1","0"], ["0","1","0","1","0"], ["0","0","0","0","0"] ]", output = 2', function () {
+            assert.equal(maximalRectangle([["0", "0", "0", "0", "0"], ["0", "1", "0", "1", "0"], ["0", "1", "0", "1", "0"], ["0", "0", "0", "0", "0"]]), 2);
+        });
+
+        it('[Leetcode example] Input: [ ["1","0","1","0","0"], ["1","0","1","1","1"], ["1","1","1","1","1"], ["1","0","0","1","0"] ]", output = 6', function () {
+            assert.equal(maximalRectangle([["1", "0", "1", "0", "0"], ["1", "0", "1", "1", "1"], ["1", "1", "1", "1", "1"], ["1", "0", "0", "1", "0"]]), 6);
+        });
+
+        it('[Huge Matrix] Input: [ 1000 x 1000 ]", output = 1 000 000 ', function () {
+
+            let hugeMatrix = new Array(1000);
+            for (let i = 0; i < 1000; i++) {
+                hugeMatrix[i] = new Array(1000).fill("1");
+            }
+
+            assert.equal(maximalRectangle(hugeMatrix), 1000 * 1000);
+        });
+    });
 });
 
 var t1 = performance.now();
