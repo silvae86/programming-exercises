37927fbca5e466e85a32bbcc902bc2a7dc4f8106
João Rocha da Silva <joaorosilva@gmail.com>
2020-08-31 19:51:49 +0100

    Started double slice problem (Codility)

---
+++ src/max-double-slice/max-double-slice.js
@@ -0,0 +1,9 @@
+// you can write to stdout for debugging purposes, e.g.
+// console.log('this is a debug message');
+
+function solution(A) {
+
+}
+
+
+exports.solution = solution;
---
+++ src/max-double-slice/max-double-slice.test.js
@@ -0,0 +1,25 @@
+var assert = require('assert');
+var solution = require("./max-double-slice").solution;
+
+const {performance} = require('perf_hooks');
+var t0 = performance.now();
+
+describe('Maximum Double Slice', function () {
+    describe('Trivial Cases', function () {
+        it('Input: []", output = 0', function () {
+            assert.equal(solution([]), 0);
+        });
+        it('Input: []", output = 0', function () {
+            assert.equal(solution([]), 0);
+        });
+    });
+
+    describe('Valid Cases', function () {
+        it('Input: [3,2,6,-1,4,5,-1,2]", output = 17', function () {
+            assert.equal(solution([3, 2, 6, -1, 4, 5, -1, 2]), 0);
+        });
+    });
+});
+
+var t1 = performance.now();
+console.log("Call to doSomething took " + (t1 - t0) + " milliseconds.");
